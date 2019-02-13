82f19fea58bc56b5c9847bf0cf24ae8baa15f667
alexisperez121 <alexisperez121@gmail.com>
2019-02-13 15:11:53 -0800

    Update CppStringToken.h

--- chapter3/wci/frontend/Cpp/tokens/CppStringToken.h
+++ chapter3/wci/frontend/Cpp/tokens/CppStringToken.h
@@ -1,15 +1,41 @@
-/*
- * CppStringToken.h
+/**
+ * <h1>CppStringToken</h1>
  *
- *  Created on: Feb 7, 2019
- *      Author: bobbyhaus
+ * <p> Cpp string tokens.</p>
+ *
+ * <p>Copyright (c) 2017 by Ronald Mak</p>
+ * <p>For instructional purposes only.  No warranties.</p>
  */
-
 #ifndef WCI_FRONTEND_CPP_TOKENS_CPPSTRINGTOKEN_H_
 #define WCI_FRONTEND_CPP_TOKENS_CPPSTRINGTOKEN_H_
 
+#include <string>
+#include "../CppToken.h"
 
+namespace wci { namespace frontend { namespace cpp { namespace tokens {
 
+using namespace std;
+using namespace wci::frontend::cpp;
 
+class CppStringToken : public CppToken
+{
+public:
+    /**
+     * Constructor.
+     * @param source the source from where to fetch the token's characters.
+     * @throw a string message if an error occurred.
+     */
+    CppStringToken(Source *source) throw (string);
+
+protected:
+    /**
+     * Extract a Pascal string token from the source.
+     * Override of wci::frontend::Token.
+     * @throw a string message if an error occurred.
+     */
+    void extract() throw (string);
+};
+
+}}}}  // namespace wci::frontend::cpp::tokens
 
 #endif /* WCI_FRONTEND_CPP_TOKENS_CPPSTRINGTOKEN_H_ */
